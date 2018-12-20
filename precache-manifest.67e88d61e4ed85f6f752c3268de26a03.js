309e83b066ba65d619e60a1d35f9290daee32669
pensadotech <armando@pensadotech.com>
2018-12-19 17:05:51 -0800

    Updates

---
+++ precache-manifest.67e88d61e4ed85f6f752c3268de26a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/pensadotech/reactgithub/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "1e613129e9d81e31906c",
+    "url": "/pensadotech/reactgithub/static/js/runtime~main.1e613129.js"
+  },
+  {
+    "revision": "7d3d5a382a287609d557",
+    "url": "/pensadotech/reactgithub/static/js/main.7d3d5a38.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/pensadotech/reactgithub/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "7d3d5a382a287609d557",
+    "url": "/pensadotech/reactgithub/static/css/main.6d405e8b.chunk.css"
+  },
+  {
+    "revision": "5b5596c7c7fb1c1488ea366e0eec0999",
+    "url": "/pensadotech/reactgithub/index.html"
+  }
+];
